bec01e8f39f7ca07ec8b4f3a0240aa10d0ee1d70
Dan Johansson <danjohansson98@gmail.com>
2024-01-26 21:36:26 +0100

    Updates data features of clip class

--- classes/clip.py
+++ classes/clip.py
@@ -4,19 +4,22 @@ import datetime
 
 @dataclass
 class Clip:
-    id: int
-    url: str
-    broadcaster_id: int
-    broadcaster_name: str
-    game_id: int
-    title: str
-    view_count: int
-    created_at: datetime.datetime
-    thumbnail_url: str
-    thumbnail_path: str
-    duration: int
-    clip_path: str
-    posted: bool = False
+    id: int = None
+    url: str = None
+    video_url: str = None
+    broadcaster_id: int = None
+    broadcaster_name: str = None
+    game_id: int = None
+    game_name: str = None
+    title: str = None
+    view_count: int = None
+    created_at: datetime.datetime = None
+    thumbnail_url: str = None
+    thumbnail_path: str = None
+    duration: int = None
+    clip_path: str = None
+    deleted: int = None
+    posted: int = None
 
     def to_json(self):
         """_summary_
@@ -27,9 +30,11 @@ class Clip:
         return {
             "id": self.id,
             "url": self.url,
+            "video_url": self.video_url,
             "broadcaster_id": self.broadcaster_id,
             "broadcaster_name": self.broadcaster_name,
             "game_id": self.game_id,
+            "game_name": self.game_name,
             "title": self.title,
             "view_count": self.view_count,
             "created_at": self.created_at.isoformat(),
@@ -37,5 +42,6 @@ class Clip:
             "duration": self.duration,
             "clip_path": self.clip_path,
             "thumbnail_path": self.thumbnail_path,
+            "deleted": self.deleted,
             "posted": self.posted,
         }
