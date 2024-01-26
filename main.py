76fa5c502e7efbac86372a3bbfb4250c3b3f5d53
Dan Johansson <danjohansson98@gmail.com>
2024-01-26 21:38:24 +0100

    Started working on main runtime file

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from classes.clip import *
+from classes.sqlite import init_db, insert_clip
+from classes.twitchclip import TwitchClips
+import os
 
 if __name__ == "__main__":
     db_name = "clips.db"
-    init_db(db_name)
+    if not os.path.exists(db_name):
+        init_db(db_name)
 
     twitch_clips = TwitchClips()
     twitch_clips.get_clip_ids()
     twitch_clips.prepare_clips()
+    twitch_clips.download_clips()
 
-    for clip in twitch_clips.clips:
-        insert_clip(db_name, clip)
+    # for clip in twitch_clips.clips:
+    #     insert_clip(db_name, clip)
