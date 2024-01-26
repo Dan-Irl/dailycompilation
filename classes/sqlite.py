4782c31232794a003fdcc79e4996cee4caa06b83
Dan Johansson <danjohansson98@gmail.com>
2024-01-26 21:37:06 +0100

    Cleanup of inseart clip method

--- classes/sqlite.py
+++ classes/sqlite.py
@@ -9,6 +9,7 @@ def init_db(db_name):
         """CREATE TABLE IF NOT EXISTS clips
                  (id INTEGER PRIMARY KEY,
                   url TEXT,
+                  video_url TEXT,
                   broadcaster_id INTEGER,
                   broadcaster_name TEXT,
                   game_id INTEGER,
@@ -30,14 +31,28 @@ def insert_clip(db_name, clip):
     c = conn.cursor()
 
     c.execute(
-        """INSERT INTO clips (id, url, broadcaster_id, broadcaster_name, game_id, title, view_count, created_at, thumbnail_url, duration, path) 
-                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+        """INSERT INTO clips 
+        (id, 
+        url,
+        video_url, 
+        broadcaster_id, 
+        broadcaster_name, 
+        game_id, 
+        game_name,
+        title, 
+        view_count, 
+        created_at, 
+        thumbnail_url, 
+        duration, 
+        path) 
+                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
         (
             clip.id,
             clip.url,
             clip.broadcaster_id,
             clip.broadcaster_name,
             clip.game_id,
+            clip.game_name,
             clip.title,
             clip.view_count,
             clip.created_at.isoformat(),
@@ -49,26 +64,3 @@ def insert_clip(db_name, clip):
 
     conn.commit()
     conn.close()
-
-
-# Example usage
-db_name = "clips.db"
-init_db(db_name)
-
-# Example Clip instance
-clip = Clip(
-    1,
-    "http://example.com",
-    123,
-    "Broadcaster",
-    456,
-    "Game Title",
-    1000,
-    datetime.datetime.now(),
-    "http://example.com/thumbnail.jpg",
-    30,
-    "/path/to/clip",
-)
-
-# Insert the clip into the database
-insert_clip(db_name, clip)
