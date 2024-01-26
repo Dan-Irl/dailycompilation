f4756caf867f56f0f864964cd39284b434444b06
dan <danjohansson98@gmail.com>
2024-01-27 00:53:05 +0100

    Added editor object to pipline

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from classes.sqlite import init_db, insert_clip
 from classes.twitchclip import TwitchClips
+from classes.editing import Edit
 import os
 
 if __name__ == "__main__":
@@ -7,10 +8,19 @@ if __name__ == "__main__":
     if not os.path.exists(db_name):
         init_db(db_name)
 
+    # TODO fix scaper fopr WSL
     twitch_clips = TwitchClips()
     twitch_clips.get_clip_ids()
     twitch_clips.prepare_clips()
+
+    # list of twitch clips
+    clips = twitch_clips.clips
+
+    # Download all clips
     twitch_clips.download_clips()
 
-    # for clip in twitch_clips.clips:
-    #     insert_clip(db_name, clip)
+    # Edit clips
+    editor = Edit()
+
+    # Edit, saves compilation and returns compilation object
+    compilation = editor.edit(twitch_clips)
