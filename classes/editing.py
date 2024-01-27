5747edc9e688178ff91c51a4dbaa2a0066e7868e
dan <danjohansson98@gmail.com>
2024-01-27 21:19:49 +0100

    Imported variables are type checked. Alignemnt of text

--- classes/editing.py
+++ classes/editing.py
@@ -15,21 +15,27 @@ from datetime import datetime
 class Edit:
     def __init__(self) -> None:
         load_dotenv()
-        self.fontsize_main = os.getenv("FONTSIZE_MAIN")
-        self.fontsize_sub = os.getenv("FONTSIZE_SUB")
-        self.text_color = os.getenv("TEXT_COLOR")
-        self.font = os.getenv("FONT")
-        self.background_color = os.getenv("BACKGROUND_COLOR")
-        self.opacity = os.getenv("OPACITY")
+        self.fontsize_main = int(os.getenv("FONTSIZE_MAIN"))
+        self.fontsize_sub = int(os.getenv("FONTSIZE_SUB"))
+        self.text_color = str(os.getenv("TEXT_COLOR"))
+        self.font = str(os.getenv("FONT"))
+        # self.background_color = os.getenv("BACKGROUND_COLOR")
+        self.background_color = (0, 0, 0)
+        self.opacity = float(os.getenv("OPACITY"))
 
         self.edited_clips = []
 
-        # Init compilation object
+        # Init video
+        self.video = None
 
+        # Init compilation object
         self.compilation = Compilation()
 
     def edit(self, clips: list[Clip]) -> Compilation:
         for clip in clips:
+            # Get the clip video file from clip path
+            clip_file = VideoFileClip(clip.clip_path).resize(newsize=(1920, 1080))
+
             # Create text clips
             main_title = TextClip(
                 f"{clip.title}",
@@ -38,7 +44,7 @@ class Edit:
                 font=self.font,
             )
             sub_title = TextClip(
-                f"{clip.title} - {clip.game_name}",
+                f"{clip.broadcaster_name} - {clip.game_name}",
                 fontsize=self.fontsize_sub,
                 color=self.text_color,
                 font=self.font,
@@ -53,31 +59,31 @@ class Edit:
             ).set_opacity(self.opacity)
 
             # Set the position of the text and the background
-            main_title = main_title.set_position(("left", 10))
-            subtitle = subtitle.set_position(("left", main_title.size[1] + 10))
-            background = background.set_position(("left", "bottom"))
+            main_title = main_title.set_position(("center", 10))
+            sub_title = sub_title.set_position(("center", main_title.size[1] + 10))
+            background = background.set_position(("center", "bottom"))
 
             # Composite the text and the background
             txt_clip = CompositeVideoClip(
-                [background, main_title, subtitle],
+                [background, main_title, sub_title],
                 size=(text_width + 20, text_height + 20),
             )
-            txt_clip = txt_clip.set_position(("center", "bottom")).set_duration(5)
+            txt_clip = txt_clip.set_position(("left", "bottom")).set_duration(5)
 
             video = CompositeVideoClip(
-                [clip, txt_clip.set_start(0)]
+                [clip_file, txt_clip.set_start(0)]
             )  # Adjust set_start for overlay timing
             self.edited_clips.append(video)
 
-            self.video = concatenate_videoclips(self.edited_clips)
-            
-            # save path
-            path = f"dailycompilation/data/compilations/{datetime.now().strftime("%y/%m/%d")}.mp4"
-            self.video.write_videofile(path)
-            
-            # Create compilation object
-            self.compilation.path = path
+        print(self.edited_clips)
 
-            return self.compilation
-            
-            
+        self.video = concatenate_videoclips(self.edited_clips)
+
+        # save path
+        path = f"dailycompilation/data/compilations/test.mp4"
+        self.video.write_videofile(path, fps=24)
+
+        # Create compilation object
+        self.compilation.path = path
+
+        return self.compilation
