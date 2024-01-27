734e3598b659925b8a3128c19b55aa592918fd30
dan <danjohansson98@gmail.com>
2024-01-27 21:18:54 +0100

    Testing of main pipeline

--- main.py
+++ main.py
@@ -10,17 +10,21 @@ if __name__ == "__main__":
 
     # TODO fix scaper fopr WSL
     twitch_clips = TwitchClips()
-    twitch_clips.get_clip_ids()
+    # twitch_clips.get_clip_ids()
+    twitch_clips.clips_id_list = [
+        "CarefulBlitheCheetahDendiFace-ecWD2KF-SFxyzHI2",
+        "CarefulBlitheCheetahDendiFace-ecWD2KF-SFxyzHI2",
+    ]
     twitch_clips.prepare_clips()
 
     # list of twitch clips
     clips = twitch_clips.clips
 
     # Download all clips
-    twitch_clips.download_clips()
+    # twitch_clips.download_clips()
 
     # Edit clips
     editor = Edit()
 
     # Edit, saves compilation and returns compilation object
-    compilation = editor.edit(twitch_clips)
+    compilation = editor.edit(twitch_clips.clips)
